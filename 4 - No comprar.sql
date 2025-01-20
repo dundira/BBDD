c46911a65cd1a399b35b8df4e42ee687373b3527
dundira <134929952+dundira@users.noreply.github.com>
2025-01-20 19:28:27 +0100

    Delete 4 - No comprar.sql

--- 4 - No comprar.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    c.idclientes, 
-    c.Cli_Nombre
-FROM 
-    clientes c
-LEFT JOIN 
-    pedidos p ON c.idclientes = p.Ped_IDCliente
-WHERE 
-    p.idpedidos IS NULL;  -- Filtramos los clientes sin pedidos (idpedidos será NULL para esos clientes)
